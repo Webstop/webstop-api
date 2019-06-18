413a6ade4679233bf11772e667217cafee33dbdf
Greg Hemphill <greg@webstop.com>
2019-06-17 21:57:08 -0400

    Users responds with object

--- lib/webstop-api/interfaces/users.rb
+++ lib/webstop-api/interfaces/users.rb
@@ -1,11 +1,13 @@
 require "webstop-api/rest/users"
+require "webstop-api/models/user"
 module WebstopApi
   module Interfaces
     module Users
       include WebstopApi::REST::Users
 
       def get_user_by_credentials(token)
-        _get_user_by_credentials(token)
+        user = _get_user_by_credentials(token)
+        User.new(user)
       end
       
     end
