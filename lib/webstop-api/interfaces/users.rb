e89f209153e3b5333e10dd9baebfc7b1527f0de5
Greg Hemphill <greg@webstop.com>
2019-06-17 22:37:08 -0400

    Users responds with object, try 3

--- lib/webstop-api/interfaces/users.rb
+++ lib/webstop-api/interfaces/users.rb
@@ -6,7 +6,8 @@ module WebstopApi
       include WebstopApi::REST::Users
 
       def get_user_by_credentials(token)
-        user = _get_user_by_credentials(token).with_indifferent_access
+        response = _get_user_by_credentials(token)
+        user = response['user'].with_indifferent_access
         User.new(user)
       end
       
