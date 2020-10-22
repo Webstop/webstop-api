e3e4096c82403fa1ec79e31d3cb9b1c979e904df
Dan Lynn <gitkraken@danlynn.org>
2020-10-22 12:42:00 -0400

    Add better console inspection of models

--- lib/webstop-api/models/base.rb
+++ lib/webstop-api/models/base.rb
@@ -9,5 +9,16 @@ module WebstopApi
       end
     end
 
+    # just a fancy inspection of the class instance that shows the class attrs
+    def inspect
+      attr_accessor_methods = self.class.public_instance_methods.select{|m| m =~ /^\w.*=$/}.sort.collect{|m| m.to_s[0...-1]}
+      longest_method_length = attr_accessor_methods.map(&:size).max
+      response = "#<#{self.class.name}:#{'0x%x' % (self.object_id << 1)}> {\n"
+      attr_accessor_methods.each do |m|
+        response += "\e[0m#{"%#{longest_method_length + 4}s" % m} \e[97m=> \e[33m#{send(m.to_sym).inspect}\e[0m\n"
+      end
+      response += "\e[0m}"
+    end
+
   end
 end
