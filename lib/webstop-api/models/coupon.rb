2b68eab773c956ec4b78732f44e2b3bd11034ebf
Dan Lynn <gitkraken@danlynn.org>
2020-11-20 12:39:43 -0500

    Add missing live_date field to Coupon model

--- lib/webstop-api/models/coupon.rb
+++ lib/webstop-api/models/coupon.rb
@@ -3,7 +3,8 @@ module WebstopApi
     attr_accessor :id, :source_coupon_id, :brand, :recipe_title, :recipe_number,
       :category, :department, :value, :title, :description, :short_description,
       :details, :fine_print, :image_path, :display_start_date, :display_end_date,
-      :expire_date, :tags, :retailer_sponsored, :min_purchase, :key_ingredient,
-      :hidden, :sharable, :completed_immigration, :status, :issuer_status
+      :live_date, :expire_date, :tags, :retailer_sponsored, :min_purchase,
+      :key_ingredient, :hidden, :sharable, :completed_immigration, :status,
+      :issuer_status
   end
 end
