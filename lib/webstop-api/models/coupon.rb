a71ddd8ad2ee0ad51baf5faa846eda8923882662
Dan Lynn <gitkraken@danlynn.org>
2020-11-19 19:06:27 -0500

    Add auto-clip required fields to Coupon model

--- lib/webstop-api/models/coupon.rb
+++ lib/webstop-api/models/coupon.rb
@@ -1,8 +1,9 @@
 module WebstopApi
   class Coupon < Base
-    attr_accessor :id, :brand, :recipe_title, :recipe_number, :category, :department,
-      :value, :title, :description, :details, :fine_print, :image_path, :display_start_date,
-      :display_end_date, :expire_date, :tags, :retailer_sponsored, :min_purchase,
-      :key_ingredient, :hidden, :sharable
+    attr_accessor :id, :source_coupon_id, :brand, :recipe_title, :recipe_number,
+      :category, :department, :value, :title, :description, :short_description,
+      :details, :fine_print, :image_path, :display_start_date, :display_end_date,
+      :expire_date, :tags, :retailer_sponsored, :min_purchase, :key_ingredient,
+      :hidden, :sharable, :completed_immigration, :status, :issuer_status
   end
 end
