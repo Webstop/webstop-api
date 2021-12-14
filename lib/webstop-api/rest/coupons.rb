40e05cc0164ab115d684ee0914776bf4f9cf47d6
Dan Lynn <gitkraken@danlynn.org>
2021-12-14 15:03:30 -0500

    Added temp debug logging

--- lib/webstop-api/rest/coupons.rb
+++ lib/webstop-api/rest/coupons.rb
@@ -33,6 +33,9 @@ module WebstopApi
       end
 
       def _search_coupons(query, options = {})
+        puts "=*= WebstopApi::VERSION: #{WebstopApi::VERSION}"
+        puts "=*= WebstopApi.api_version: #{WebstopApi.api_version.inspect}"
+        puts "=*= retailer_connection.api_version: #{retailer_connection.api_version}"
         coupons = retailer_connection.get do |req|
           query_param = ERB::Util.url_encode(query.to_json)
           if WebstopApi.api_version =~ /v3/i
@@ -40,7 +43,9 @@ module WebstopApi
           else
             credentials_name = 'api_user_credentials'
           end
+          puts "=*= credentials_name: #{credentials_name.inspect}"
           req.url "coupons/search.json?#{credentials_name}=#{options[:token]}&query=#{query_param}"
+          puts "=*= req.url: #{req.url.inspect}"
         end
         if coupons.status != 200
           raise JSON.parse(coupons.body)['errors'].join(',')
